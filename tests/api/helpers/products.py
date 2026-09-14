29c7503bfd4dfff20c74b6bf2e8563c55abc70b4
ABalchugov <inspektr1994@gmail.com>
2026-09-14 20:21:49 +0300

    fix: вынесены функции создания и удаления продукта в helpers

---
+++ tests/api/helpers/products.py
@@ -0,0 +1,30 @@
+import requests
+
+PRODUCT = {
+    "name": "iPhone 17",
+    "description": "Описание для Apple iPhone 17",
+    "price": 1300,
+    "category": "Phones",
+    "manufacturer": "Apple",
+    "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYLwvcBDB1Qn4UvFrL2zdEJRlBPxe76yVnfZG1mEhFYNtsXHKtddZXfK3X&s=10",
+    "freeShipping": True
+}
+
+
+def create_product(url):
+    response = requests.post(f"{url}/add-product", json=PRODUCT)
+
+    print("\nStatus code:", response.status_code)
+    print("Headers:", response.headers)
+    print("Body:", response.text)
+
+    return {"body": response.text, "status": response.status_code}
+
+def delete_product(url, product_id):
+    delete_response = requests.delete(f"{url}/products/id/{product_id}")
+
+    print("\nStatus code:", delete_response.status_code)
+    print("Headers:", delete_response.headers)
+    print("Body:", delete_response.text)
+
+    return {"body": delete_response.text, "status": delete_response.status_code}
--- tests/api/test_products.py
+++ tests/api/test_products.py
@@ -3,6 +3,7 @@ import allure
 import requests
 from jsonschema import validate
 
+from helpers.products import create_product, delete_product
 from schemas.products_schema import products_schema
 from dotenv import load_dotenv
 
@@ -33,31 +34,13 @@ def test_get_products():
 @allure.title("Добавление нового продукта")
 @allure.severity(allure.severity_level.NORMAL)
 def test_add_product():
-    request_body = {
-        "name": "iPhone 17",
-        "description": "Описание для Apple iPhone 17",
-        "price": 1300,
-        "category": "Phones",
-        "manufacturer": "Apple",
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYLwvcBDB1Qn4UvFrL2zdEJRlBPxe76yVnfZG1mEhFYNtsXHKtddZXfK3X&s=10",
-        "freeShipping": True
-    }
-    response = requests.post(f"{API_URL}/add-product", json=request_body)
+    response = create_product(API_URL)
+    assert response["status"] == 200
+    assert response["body"].startswith("Продукт успешно добавлен с ID:")
 
-    print("\nStatus code:", response.status_code)
-    print("Headers:", response.headers)
-    print("Body:", response.text)
-
-    assert response.status_code == 200
-
-    body = response.text
-    assert body.startswith("Продукт успешно добавлен с ID:")
-
-    product_id = int(body.replace("Продукт успешно добавлен с ID:", "").strip())
-
-    delete_response = requests.delete(f"{API_URL}/products/id/{product_id}")
-
-    assert delete_response.status_code == 200
+    product_id = int(response["body"].replace("Продукт успешно добавлен с ID:", "").strip())
+    delete_response = delete_product(API_URL, product_id)
+    assert delete_response["status"] == 200
 
 
 @allure.epic("API Automation")
@@ -65,31 +48,15 @@ def test_add_product():
 @allure.title("Удаление продукта по ID")
 @allure.severity(allure.severity_level.NORMAL)
 def test_delete_product():
-    request_body = {
-        "name": "iPhone 17",
-        "description": "Описание для Apple iPhone 17",
-        "price": 1300,
-        "category": "Phones",
-        "manufacturer": "Apple",
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYLwvcBDB1Qn4UvFrL2zdEJRlBPxe76yVnfZG1mEhFYNtsXHKtddZXfK3X&s=10",
-        "freeShipping": True
-    }
-    response = requests.post(f"{API_URL}/add-product", json=request_body)
+    response = create_product(API_URL)
+    assert response["status"] == 200
 
-    assert response.status_code == 200
+    product_id = int(response["body"].replace("Продукт успешно добавлен с ID:", "").strip())
 
-    body = response.text
+    delete_response = delete_product(API_URL, product_id)
 
-    product_id = int(body.replace("Продукт успешно добавлен с ID:", "").strip())
-
-    delete_response = requests.delete(f"{API_URL}/products/id/{product_id}")
-
-    print("\nStatus code:", delete_response.status_code)
-    print("Headers:", delete_response.headers)
-    print("Body:", delete_response.text)
-
-    assert delete_response.status_code == 200
-    assert delete_response.text == "Товар удалён"
+    assert delete_response["status"] == 200
+    assert delete_response["body"] == "Товар удалён"
 
 
 @allure.epic("API Automation")
@@ -97,23 +64,10 @@ def test_delete_product():
 @allure.title("Частичное обновление товара по ID")
 @allure.severity(allure.severity_level.NORMAL)
 def test_patch_product():
-    request_body = {
-        "name": "iPhone 17",
-        "description": "Описание для Apple iPhone 17",
-        "price": 1300,
-        "category": "Phones",
-        "manufacturer": "Apple",
-        "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYLwvcBDB1Qn4UvFrL2zdEJRlBPxe76yVnfZG1mEhFYNtsXHKtddZXfK3X&s=10",
-        "freeShipping": True
-    }
-    response = requests.post(f"{API_URL}/add-product", json=request_body)
+    response = create_product(API_URL)
+    assert response["status"] == 200
 
-    assert response.status_code == 200
-
-    body = response.text
-
-    product_id = int(body.replace("Продукт успешно добавлен с ID:", "").strip())
-
+    product_id = int(response["body"].replace("Продукт успешно добавлен с ID:", "").strip())
     patch_response = requests.patch(f"{API_URL}/products/id/{product_id}", json={"description": "Новое описание"})
 
     print("\nStatus code:", patch_response.status_code)
@@ -123,6 +77,6 @@ def test_patch_product():
     assert patch_response.status_code == 200
     assert patch_response.text == "Товар частично обновлён"
 
-    delete_response = requests.delete(f"{API_URL}/products/id/{product_id}")
+    delete_response = delete_product(API_URL, product_id)
 
-    assert delete_response.status_code == 200
+    assert delete_response["status"] == 200
