c61778a12d40bb2afcce599064fdf1c284210ee1
ABalchugov <inspektr1994@gmail.com>
2026-09-16 20:21:08 +0300

    feat: jenkins

--- conftest.py
+++ conftest.py
@@ -22,7 +22,7 @@ def pytest_addoption(parser):
     parser.addoption(
         "--browser_version",
         default="148.0",
-        choices=("151.0", "150.0", "149.0", "148.0"),
+        choices=("152.0", "151.0", "148.0", "153.0", "154.0"),
         help="Browser version to use"
     )
     parser.addoption(
