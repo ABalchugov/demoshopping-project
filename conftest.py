75d70bb5fb290af1521341e6e129fba06ab46829
ABalchugov <inspektr1994@gmail.com>
2026-09-16 20:10:51 +0300

    feat: jenkins

--- conftest.py
+++ conftest.py
@@ -1,6 +1,10 @@
 import pytest
 from selenium import webdriver
+from utils import attach
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from dotenv import load_dotenv
+import os
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -8,9 +12,77 @@ def load_env():
     load_dotenv()
 
 
-@pytest.fixture(scope="function")
-def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser",
+        default="chrome",
+        choices=("chrome", "firefox"),
+        help="Browser to use"
+    )
+    parser.addoption(
+        "--browser_version",
+        default="148.0",
+        choices=("151.0", "150.0", "149.0", "148.0"),
+        help="Browser version to use"
+    )
+    parser.addoption(
+        "--headless",
+        choices=["true", "false"],
+        default="false",
+    )
+    parser.addoption(
+        "--window_size",
+        default="1920*1080",
+        help="Window size"
+    )
+
+
+@pytest.fixture(scope="function", autouse=True)
+def driver(request):
+    browser = request.config.getoption("--browser")
+    browser_version = request.config.getoption("--browser_version")
+    headless = request.config.getoption("--headless").lower() == "true"
+    window_size = request.config.getoption("--window_size")
+    width_str, height_str = window_size.split("*", 1)
+
+    width = int(width_str) if width_str else 1920
+    height = int(height_str) if height_str else 1080
+
+    options = None
+
+    if browser == "chrome":
+        options = ChromeOptions()
+        options.add_argument(f"--window-size={width},{height}")
+    elif browser == "firefox":
+        options = FirefoxOptions()
+        options.add_argument(f"--width={width}")
+        options.add_argument(f"--height={height}")
+
+    if headless:
+        options.add_argument("--headless")
+
+    selenoid_capabilities = {
+        "browserName": browser,
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+    driver = webdriver.Remote(
+        command_executor=f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options=options
+    )
+
     yield driver
+
+    attach.add_html(driver)
+    attach.add_screenshot(driver)
+    attach.add_logs(driver)
+    attach.add_video(driver)
+
     driver.quit()
---
+++ utils/attach.py
@@ -0,0 +1,30 @@
+import os
+import allure
+from allure_commons.types import AttachmentType
+
+
+# Скриншоты
+def add_screenshot(driver):
+    png = driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+# логи
+def add_logs(driver):
+    log = "".join(f'{text}\n' for text in driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+
+
+# html-код страницы
+def add_html(driver):
+    html = driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+
+
+def add_video(driver):
+    selenoid_url = os.getenv("SELENOID_URL")
+    video_url = f"https://{selenoid_url}/video/" + driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + driver.session_id, AttachmentType.HTML, '.html')
