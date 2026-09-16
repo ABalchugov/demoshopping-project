2252b246f5941186795423f0fe120c419813310c
ABalchugov <inspektr1994@gmail.com>
2026-09-16 20:37:59 +0300

    fix: jenkins

--- utils/attach.py
+++ utils/attach.py
@@ -1,6 +1,7 @@
 import os
 import allure
 from allure_commons.types import AttachmentType
+from selenium.common import WebDriverException
 
 
 # Скриншоты
@@ -8,12 +9,15 @@ def add_screenshot(driver):
     png = driver.get_screenshot_as_png()
     allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
 
-
-# логи
 def add_logs(driver):
-    log = "".join(f'{text}\n' for text in driver.get_log(log_type='browser'))
-    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+    if driver.capabilities.get("browserName") != "chrome":
+        return
 
+    try:
+        log = "".join(f'{text}\n' for text in driver.get_log(log_type='browser'))
+        allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+    except WebDriverException:
+        pass
 
 # html-код страницы
 def add_html(driver):
